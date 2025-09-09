5ab1bcc6b8cbfba56913315ef96291013877f8f9
Arthur Klober <arthurciszk@gmail.com>
2025-09-08 21:02:52 -0300

    Implementação completa do desafio POO com Nokia e Iphone

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone c1 = new Iphone("99225566", "16 Pro Max", "2M240SN2094N2KJ3N4", 256);
+c1.ReceberLigacao();
+c1.Ligar();
+c1.InstalarAplicativo("WhatsApp");
+
+Nokia c2 = new Nokia("77884433", "Tijolão", "5W2HSN2094N2KJ3N4", 16);
+c2.ReceberLigacao();
+c2.Ligar();
+c2.InstalarAplicativo("Telegram");
